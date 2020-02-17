695e8d0641e77df337cc9d49912fcaa6c0392fbe
dayluke <dayluke01@gmail.com>
2020-02-17 11:00:57 +0000

    Added base assets to scene, including a 'fan', 'ball' and 'obstacle'.

---
+++ Assets/Scripts/FanController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FanController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody ball = null;
+    [SerializeField] private float force = 5f;
+
+    private void Update()
+    {
+        // Change this to a slider?
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ball.velocity += Vector3.up * force;
+        }
+    }
+}
