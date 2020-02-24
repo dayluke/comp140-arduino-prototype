b6fd1b5f3265849a3f9fdf52df3b9bc043b8cc1b
dayluke <dayluke01@gmail.com>
2020-02-24 09:58:16 +0000

    Added a slider to change the fans speed.

--- Assets/Scripts/FanController.cs
+++ Assets/Scripts/FanController.cs
@@ -4,6 +4,7 @@ public class FanController : MonoBehaviour
 {
     [SerializeField] private Rigidbody ball = null;
     [SerializeField] private float force = 5f;
+    private float currSlider = 0f;
 
     private void Update()
     {
@@ -12,5 +13,14 @@ public class FanController : MonoBehaviour
         {
             ball.velocity += Vector3.up * force * Time.deltaTime;
         }
+        else
+        {
+            ball.velocity += Vector3.up * force * currSlider * Time.deltaTime;
+        }
+    }
+
+    public void SliderChanged(float sliderValue)
+    {
+        currSlider = sliderValue;
     }
 }
---
+++ Assets/Scripts/SliderMapper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SliderMapper : MonoBehaviour
+{
+    public FanController fan;
+    public int maxValue;
+    public int minValue;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="value">a float value between 0 and 1</param>
+    public void OnValueChanged(float value)
+    {
+        float newValue = Remap(value, 0, 1, minValue, maxValue);
+        fan.SliderChanged(newValue);
+    }
+
+    private float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+}
