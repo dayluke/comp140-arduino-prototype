5c020aa9c472015f3efde64c4373a92d4f458928
dayluke <dayluke01@gmail.com>
2020-02-24 09:59:17 +0000

    Added a random height to the obstacles.

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,14 +4,25 @@ public class Obstacle : MonoBehaviour
 {
     [SerializeField] private Vector3 endPoint = Vector3.zero;
     [SerializeField] private float speed = 5f;
-    public bool isTopObstacle = true;
+    [SerializeField] public bool isTopObstacle = true;
+    [SerializeField] private float minHeight = 0.5f;
+    [SerializeField] private float maxHeight = 5f;
+    // [SerializeField] private float spaceBetween; // <- Spawn two obstacles every time??
 
     private void Start()
     {
         if (!isTopObstacle)
         {
-            transform.position -= (Vector3.up * (transform.position.y - 1.5f));
+            transform.position -= (Vector3.up * (transform.position.y - 0.5f));
         }
+        RandomiseHeight();
+    }
+
+    private void RandomiseHeight()
+    {
+        float height = Random.Range(minHeight, maxHeight);
+        transform.localScale += Vector3.up * height;
+        transform.position += isTopObstacle ? -Vector3.up * (height / 2) : Vector3.up * (height / 2);
     }
 
     private void Update()
